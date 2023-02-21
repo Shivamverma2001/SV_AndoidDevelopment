8d07012f0e41b394528c41d13985988ce8df1e93
Shivam Verma <shivamverma2001sj@gmail.com>
2023-02-21 13:55:11 +0530

    commit

--- CSE225/app/src/main/java/com/example/cse225/c15_Notification.kt
+++ CSE225/app/src/main/java/com/example/cse225/c15_Notification.kt
@@ -8,9 +8,12 @@ import android.app.RemoteInput
 import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.graphics.ColorSpace.RenderIntent
 import android.media.AudioAttributes
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -39,12 +42,47 @@ class c15_Notification : AppCompatActivity() {
         notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         btnNotify.setOnClickListener {
             val intent =Intent(this, c15_b::class.java)
-            pendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+            pendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_IMMUTABLE)
             soundUri= Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE+"://"+applicationContext.packageName+"/"+R.raw.ringtone)
             audioAttr=AudioAttributes.Builder()
                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                 .setUsage(AudioAttributes.USAGE_NOTIFICATION)
                 .build()
+            //remoteCollapsedViews= RemoteViews(packageName,R.layout.activity_c92)
+            //remoteCollapsedViews=RemoteViews(packageName,R.layout.activity_c9_splash_screen)
+            myNotificationChannel()
+            /*remoteInput=RemoteInput.Builder(myKey).setLabel("Replying...").build()
+            val action:Notification.Action=Notification.Action.Builder(R.drawable.check,"Reply",pendingIntent)
+                .addRemoteInput(remoteInput).build()
+            builder.addAction(action)*/
+            notificationManager.notify(notificationId,builder.build())
+        }
+    }
+    private fun myNotificationChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            notificationChannel= NotificationChannel(channelId,description,NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor= Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationChannel.setSound(soundUri,audioAttr)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder=Notification.Builder(this,channelId)
+                .setSmallIcon(R.drawable.baseline_announcement_24)
+                .setContentTitle(title)
+                .setContentText(description)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.logo_toolbar))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+        }
+        else{
+            builder=Notification.Builder(this)
+                .setSmallIcon(R.drawable.baseline_announcement_24)
+                .setContentTitle(title)
+                .setContentText(description)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.logo_toolbar))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
         }
     }
 }
--- CSE225/app/src/main/java/com/example/cse225/c15_b.kt
+++ CSE225/app/src/main/java/com/example/cse225/c15_b.kt
@@ -1,11 +1,22 @@
 package com.example.cse225
 
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class c15_b : AppCompatActivity() {
+    lateinit var notificationManager:NotificationManager
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_c15_b)
+        var txtView=findViewById<TextView>(R.id.tv)
+        var inp=c15_Notification()
+        txtView.text="hey"
+        notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(inp.notificationId)
     }
 }
